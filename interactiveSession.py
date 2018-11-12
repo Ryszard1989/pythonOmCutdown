15727fba7dd1c716646c5403c1c0ae42d476877b
Ryszard Szulc <rys.szulc@gmail.com>
2018-11-12 22:40:52 +0000

    Add portfolio and total value of portfolio and cash. Display to user.

--- interactiveSession.py
+++ interactiveSession.py
@@ -7,15 +7,14 @@ class InteractiveSession:
         self.stockExchange = stockExchange
         userName = input("Hi trader, what's your name?\n")
         self.portfolio = Portfolio(userName)
-
+        self.dayCount = 1
         print("Play till 5 days have passed see if your portfolio prices are better than the market's!\n")
         print("Trading has begun!")
 
     def session(self):
         self.stockExchange.printStockExchange()
         validMenuOptions = ['T','t','V','v','P','p','Q','q']
-        dayCount = 1
-        while(dayCount <= 5):
+        while(self.dayCount <= 5):
             print("------------------------------------------------------")
             print("'T' - Trade")
             print("'V' - View Market")
@@ -28,34 +27,13 @@ class InteractiveSession:
             else:
                 # Trade
                 if menuOption in ['T','t']:
-                    print("Time to make today's trades.")
-                    self.stockExchange.printStockExchange()
-                    securitySelection = int(input("Choose the number of the stock you want to trade\n"))
-                    if(securitySelection > len(self.stockExchange.securities)):
-                        print("Sorry, that's not a valid stock\n")
-                    else:
-                        securityAmount = int(input("How much do you want to buy/sell? (use minus sign to sell)\n"))
-                        self.portfolio.makeTrade(self.stockExchange.securities[securitySelection-1], securityAmount)
-                    userinput = input("Are you done trading for today? (Choose y/n)\n")
-                    if userinput in ['Y','y','N','n']:
-                        if userinput in ['Y','y']:
-                            print("Ok that's it for today. Let's see how that goes tomorrow\n")                            
-                            dayCount += 1
-                            self.stockExchange.simulateDay()
-                            
-                        elif userinput in ['N','n']:
-                            print("Back to the menu then\n")
-                    else:
-                        print("Hmmm, that wasn't a yes or a no. We'll assume you're done. Let's see how that goes tomorrow\n")                            
-                        self.stockExchange.simulateDay()
-
+                    self.userTrade()                   
                 # View Market
                 elif menuOption in ['V','v']:
                     print("View today's market")
                     self.stockExchange.printStockExchange()
                 # View Portfolio
                 elif menuOption in ['P','p']:
-
                     print("View portfolio for Day " + str(self.stockExchange.day))
                     self.portfolio.printPortfolio()
                 # Quit
@@ -65,3 +43,24 @@ class InteractiveSession:
         print("Your final portfolio:")
         self.portfolio.printPortfolio()
         print("\nThanks for playing!\n")
+
+    def userTrade(self):
+        print("Time to make today's trades.")
+        self.stockExchange.printStockExchange()
+        securitySelection = int(input("Choose the number of the stock you want to trade\n"))
+        if(securitySelection > len(self.stockExchange.securities)):
+            print("Sorry, that's not a valid stock\n")
+        else:
+            securityAmount = int(input("How much do you want to buy/sell? (use minus sign to sell)\n"))
+            self.portfolio.makeTrade(self.stockExchange.securities[securitySelection-1], securityAmount)
+            userinput = input("Are you done trading for today? (Choose y/n)\n")
+            if userinput in ['Y','y','N','n']:
+                if userinput in ['Y','y']:
+                    print("Ok that's it for today. Let's see how that goes tomorrow\n")                            
+                    self.dayCount += 1
+                    self.stockExchange.simulateDay()                            
+                elif userinput in ['N','n']:
+                    print("Back to the menu then\n")
+                else:
+                    print("Hmmm, that wasn't a yes or a no. We'll assume you're done. Let's see how that goes tomorrow\n")                            
+                    self.stockExchange.simulateDay()
--- portfolio.py
+++ portfolio.py
@@ -10,6 +10,8 @@ class Portfolio:
         self.positions = []
         self.portfolioManager = portfolioManager
         self.cash = 10000 #TODO pass in value
+        self.portfolioValue = 0 # Value of stocks in portfolio
+        self.totalValue = 10000 # Total value of stocks + cash
             
     def calculateNewPositionAveragePrice(self, positionSecurity, positionAmount, tradeSecurity, tradeAmount):
         totalAmount = positionAmount + tradeAmount
@@ -19,7 +21,18 @@ class Portfolio:
         tradeWeight = tradeSecurity.price * tradeAmount
         weightedAverage = (positionWeight + tradeWeight) / totalAmount
         return weightedAverage
-        
+
+    #TODO - Look at changing this to just calculate delta rather than full recalc
+    def calculatePortfolioValue(self):
+        newPortfolioValue = 0
+        for p in self.positions:
+            newPortfolioValue += (p.security.price * p.amount)
+        self.portfolioValue = newPortfolioValue
+
+    #TODO - Look at changing this to just calculate delta rather than full recalc
+    def calculateTotalValue(self):
+        self.calculatePortfolioValue() #Make sure portfolio value always up to date. Look at killing unnescary recalc later.
+        self.totalValue = self.portfolioValue + self.cash
 
     def makeTrade(self, security, amount):
         if amount is 0:
@@ -36,18 +49,18 @@ class Portfolio:
                     p.security.price = newAveragePrice
                     if p.amount is 0:
                         self.positions.remove(p)
-                    self.cash -= (security.price * amount)
         if foundFlag == False:
             newSecurity = Security(security.securityName, security.securityType, security.price)
             self.positions.append(Position(newSecurity, amount))
-            self.cash -= (security.price * amount)
-
+        self.cash -= (security.price * amount)
+        self.calculatePortfolioValue()
+        self.calculateTotalValue()
 
 
     def printPortfolio(self):
         print(self.portfolioManager + "'s Portfolio:")
+        print("Total Value: " + str(self.totalValue))
+        print("Value of stocks in portfolio: " + str(self.portfolioValue))
         print("Available cash: " + str(self.cash))
         for p in self.positions:
             print("Security: " + p.security.securityName + ", Amount: " + str(p.amount) + ", AveragePrice: " + str(p.security.price))
-
-
